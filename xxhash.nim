81ce63ad0cdca42e427e36553cf3312ee8663d3c
Bruce Doan <rgv151@gmail.com>
2015-07-27 12:32:04 +0700

    first version

---
+++ xxhash.nim
@@ -0,0 +1,24 @@
+{.compile: "lib/xxhash.c".}
+
+
+proc XXH32*(input: cstring, length: csize, seed: cuint): cuint {.cdecl, importc: "XXH32", header: "../lib/xxhash.h".}
+proc XXH64*(input: cstring, length: csize, seed: culonglong): culonglong  {.cdecl, importc: "XXH32", header: "../lib/xxhash.h".}
+
+
+proc XXH32*(input: string, seed: cuint): uint32 {.inline.} =
+  XXH32(input.cstring, input.len, seed)
+
+proc XXH64*(input: string, seed: cuint): uint64{.inline.} =
+  XXH32(input.cstring, input.len, seed)
+
+proc XXH32*(input: string): uint32 {.inline.} =
+  XXH32(input.cstring, input.len, 0)
+
+proc XXH64*(input: string): uint64 {.inline.} =
+  XXH32(input.cstring, input.len, 0)
+
+
+when isMainModule:
+  assert 3794352943.uint32 == XXH32("Nobody inspects the spammish repetition")
+  assert 1740325085.uint64 == XXH64("xxhash", 20141025)
+  echo 1740325085.uint64
---
+++ xxhash.nimble
@@ -0,0 +1,9 @@
+[Package]
+name          = "xxhash"
+version       = "0.1.0"
+author        = "Huy Doan"
+description   = "xxhash wrapper for Nim"
+license       = "MIT"
+
+[Deps]
+Requires: "nim >= 0.10.0"
